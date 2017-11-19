e0ca02eea03984570ddab0ee3bbd418dfba035b7
Sean Annunciation <sannunc1@binghamton.edu>
2017-11-19 00:16:09 +0000

    Structure of VisitorI created

---
+++ fileVisitors/src/fileVisitors/visitor/VisitorI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nishant_shah_sean_annunciation_assign5.fileVisitors.src.fileVisitors.visitor;
+
+/**
+ *
+ * @author annse
+ */
+public interface VisitorI {
+    
+    public void visit(PopulateVisitor populatevistor);
+    public void visit(PalindromeHighlight palindromehighlight);
+    public void visit(PrimeLength primelength);
+    public void visit(PrintTree printtree);
+    
+    
+    
+}

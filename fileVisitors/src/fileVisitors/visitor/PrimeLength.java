703f6bb268f63d9d9b18d0db678536bcaa7f26da
Nishant Shah <nshah27@binghamton.edu>
2017-11-19 18:58:25 +0000

    PrimeLength.java updated with javadocs

--- fileVisitors/src/fileVisitors/visitor/PrimeLength.java
+++ fileVisitors/src/fileVisitors/visitor/PrimeLength.java
@@ -5,17 +5,19 @@ import fileVisitors.util.Node;
 import fileVisitors.util.TreeBuilder;
 import fileVisitors.util.VisitorHelper;
 
-/**
- *
- * @author annse
- */
-public class PrimeLength implements VisitorI {
+public class PrimeLength implements visitorI {
 	
 	private FileProcessor fp = null;
 	private TreeBuilder tb = null;
 	private String word = null;
-    private VisitorHelper vhelper=new VisitorHelper();
 	
+	private VisitorHelper vh = new VisitorHelper();
+	
+	/**
+	 * PrimeLength constructor
+	 * @param fpI - FileProcessor reference
+	 * @param tbI - TreeBuilder reference
+	 */
 	public PrimeLength (FileProcessor fpI, TreeBuilder tbI) {
 		fp = fpI;
 		tb = tbI;
@@ -25,16 +27,16 @@ public class PrimeLength implements VisitorI {
 		inorderVal(tb.getRoot());
 	}
 	
+	/**
+	 * @param r - root node for traversing
+	 */
 	private void inorderVal(Node r) {
 		if (r != null) {
 			inorderVal(r.getLeft());
-			if (vhelper.isLengthPrime(r.getWord())) {
+			if (vh.isLengthPrime(r.getWord())) {
 				r.setWord(r.getWord() + "-PRIME");
 			}
 			inorderVal(r.getRight());
 		}
 	}
-
-    
-
-}  
+}

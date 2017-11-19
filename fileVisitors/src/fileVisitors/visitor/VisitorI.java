c651790febfe74992c599fbe1c1492f4a6946e8f
Nishant Shah <nshah27@binghamton.edu>
2017-11-19 19:14:16 +0000

    final visitor interface.

--- fileVisitors/src/fileVisitors/visitor/VisitorI.java
+++ fileVisitors/src/fileVisitors/visitor/VisitorI.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package nishant_shah_sean_annunciation_assign5.fileVisitors.src.fileVisitors.visitor;
+package fileVisitors.visitor;
 
-/**
- *
- * @author annse
- */
+import fileVisitors.util.TreeBuilder;
+
 public interface VisitorI {
-    
-    public void visit(TreeBuilder treebuilder);
-      
+	
+	public void visit(TreeBuilder treeBuild);
+
 }

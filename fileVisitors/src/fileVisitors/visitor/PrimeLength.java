226b0c5c017c75c575e3dd96c58c212bf7cd869d
Sean Annunciation <sannunc1@binghamton.edu>
2017-11-19 16:08:05 +0000

    PrimeLength updated with object of Visitor Helper class and prime value check

--- fileVisitors/src/fileVisitors/visitor/PrimeLength.java
+++ fileVisitors/src/fileVisitors/visitor/PrimeLength.java
@@ -1,10 +1,20 @@
 package fileVisitors.visitor;
 
-public class PrimeLength implements visitorI {
+import fileVisitors.util.FileProcessor;
+import fileVisitors.util.Node;
+import fileVisitors.util.TreeBuilder;
+import fileVisitors.util.VisitorHelper;
+
+/**
+ *
+ * @author annse
+ */
+public class PrimeLength implements VisitorI {
 	
 	private FileProcessor fp = null;
 	private TreeBuilder tb = null;
 	private String word = null;
+    private VisitorHelper vhelper=new VisitorHelper();
 	
 	public PrimeLength (FileProcessor fpI, TreeBuilder tbI) {
 		fp = fpI;
@@ -18,10 +28,13 @@ public class PrimeLength implements visitorI {
 	private void inorderVal(Node r) {
 		if (r != null) {
 			inorderVal(r.getLeft());
-			if (isLengthPrime(r.getWord())) {
+			if (vhelper.isLengthPrime(r.getWord())) {
 				r.setWord(r.getWord() + "-PRIME");
 			}
 			inorderVal(r.getRight());
 		}
 	}
-}
+
+    
+
+}  

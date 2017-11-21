f952334ce01ffda1e4979054d62079aa7ba60fea
Nishant Shah <nshah27@binghamton.edu>
2017-11-21 04:31:35 +0000

    added writeToFile to PrintTree as suggested for this assignment

--- fileVisitors/src/fileVisitors/visitor/PrintTree.java
+++ fileVisitors/src/fileVisitors/visitor/PrintTree.java
@@ -1,28 +1,49 @@
 package fileVisitors.visitor;
 
+import fileVisitors.util.Node;
+import fileVisitors.util.TreeBuilder;
+import java.io.FileNotFoundException;
 import fileVisitors.store.Results;
+import fileVisitors.util.MyLogger;
 
-public class PrintTree implements visitorI {
+public class PrintTree implements VisitorI {
 	
-	private FileProcessor fp = null;
-	private TreeBuilder tb = null;
-	private Results r = null;
-	private String word = null;
+	private Results rs = null;
 	
-	public PrintTree (FileProcessor fpI, TreeBuilder tbI) {
-		fp = fpI;
-		tb = tbI;
-		r = rI;
+	/**
+	 * @param rsI - Results reference
+	 */
+	public PrintTree (Results rsI) {
+		MyLogger.writeMessage("PrintTree constructor called", MyLogger.DebugLevel.CONSTRUCTOR);
+		rs = rsI;
 	}
 
-	public void visit() {
+	/**
+	 * @param tb - TreeBuilder reference
+	 */
+	public void visit(TreeBuilder tb) {
+		MyLogger.writeMessage("PrintTree's visit() method called", MyLogger.DebugLevel.IN_RUN);
 		inorderVal(tb.getRoot());
+
+		rs.writeToStdout();
+		
+		try {
+			rs.writeToFile();
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("Output file cannot be generated");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 	
+	/**
+	 * @param r - root node for traversing
+	 */
 	private void inorderVal(Node r) {
 		if (r != null) {
 			inorderVal(r.getLeft());
-			r.storeNewResult(r.getWord());
+			rs.storeNewResult(r.getWord());
 			inorderVal(r.getRight());
 		}
 	}
